42a7ee21c555502c9deaf027905429cfdf336ba1
Danil <danil.zaychenko@headworks.com.ua>
2022-03-28 10:41:12 +0300

    add tabs from feach ui

--- src/components/Sidebar/SidebarTabs/SidebarTabs.tsx
+++ src/components/Sidebar/SidebarTabs/SidebarTabs.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
-import CustomButton from "src/components/CustomButton";
+import { useTranslation } from "next-i18next";
+
+import { Tabs, TabList, Tab } from "@reach/tabs";
 
 import { sidebarTabs } from "constant/sidebarTabs";
+import { categoriesSvg } from "constant/categoriesSvg";
 
 import styles from "./SidebarTabs.module.css";
+import "@reach/tabs/styles.css";
 
 interface SidebarTabsProps {
   currentTab: string;
@@ -14,31 +18,29 @@ export const SidebarTabs: FC<SidebarTabsProps> = ({
   currentTab,
   handleChange,
 }) => {
+  const { t } = useTranslation();
+
   const tabs = sidebarTabs["architectures"];
   return (
     <>
-      {tabs.map((_, index) => {
-        const currentIndex = index === 1 ? index + 1 : index + 2;
-        const tab = tabs[index === 0 ? index : currentIndex];
-        const nextTab = tabs[index === 0 ? index + 1 : currentIndex + 1];
+      <Tabs className={styles["SidebarTabs"]}>
+        <TabList className={styles["SidebarTabs-List"]}>
+          {tabs.map(({ text, type }, index) => (
+            <Tab
+              key={index}
+              className={styles["SidebarTabs-Tab"]}
+              onClick={() => {
+                console.log("s");
 
-        return !nextTab || !tab ? null : (
-          <div key={index} className={styles["SidebarTabs"]}>
-            <CustomButton
-              text={tab.text}
-              iconType={tab.type}
-              onClick={() => handleChange(tab.type)}
-              isActive={tab.type === currentTab}
-            />
-            <CustomButton
-              text={nextTab.text}
-              iconType={nextTab.type}
-              onClick={() => handleChange(nextTab.type)}
-              isActive={nextTab.type === currentTab}
-            />
-          </div>
-        );
-      })}
+                handleChange(type);
+              }}
+            >
+              {categoriesSvg[type]}
+              <span>{text}</span>
+            </Tab>
+          ))}
+        </TabList>
+      </Tabs>
     </>
   );
 };
