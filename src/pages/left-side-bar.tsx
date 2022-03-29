27f79f4951f50f10636c0d3bae74887d83cf8afb
tatianaKonovalova <tatiana.konovalova24@gmail.com>
2022-03-29 09:55:40 +0300

    added subheader for favorite panel

--- src/pages/left-side-bar.tsx
+++ src/pages/left-side-bar.tsx
@@ -1,12 +1,19 @@
+import FavouriteHeader from "src/components/FavoriteHeader/FavoriteHeader";
 import Navbar from "src/components/Navbar";
 import SidebarWithAvatar from "src/components/SidebarWithAvatar/SidebarWithAvatar";
+import styles from '../components/Layout/Layout.module.css';
 
 const LeftSideBar = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <SidebarWithAvatar />
-    </div>
+      <main className={styles["Layout-Container"]}>
+        <SidebarWithAvatar />
+        <div className={styles["Layout-Content"]} style={{padding: "0px"}}>
+          <FavouriteHeader />
+        </div>
+      </main>
+  </>
   );
 };
 
