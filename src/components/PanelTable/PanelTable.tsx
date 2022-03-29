ed3e1bb7ee7769ca1805b6ad5e9164012df45c73
Danil <danil.zaychenko@headworks.com.ua>
2022-03-29 18:58:27 +0300

    added lib for table

--- src/components/PanelTable/PanelTable.tsx
+++ src/components/PanelTable/PanelTable.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
 
 import ActionFilters from "./ActionFilters/ActionFilters";
@@ -7,6 +8,83 @@ import styles from "./PanelTable.module.css";
 import "@reach/tabs/styles.css";
 
 const PanelTable = () => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "First Name",
+        sticky: "left",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        sticky: "left",
+        accessor: "lastName",
+      },
+      {
+        Header: "Age",
+        sticky: "left",
+
+        accessor: "age",
+      },
+      {
+        Header: "Visits",
+        accessor: "visits",
+      },
+      {
+        Header: "Progress",
+        accessor: "progress",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+    ],
+    []
+  );
+
+  const data = [
+    {
+      firstName: "patch11111111111111111",
+      lastName: "psychology",
+      age: 28,
+      visits: 42,
+      progress: 21,
+      status: "complicated",
+    },
+    {
+      firstName: "cord",
+      lastName: "extent",
+      age: 10,
+      visits: 2,
+      progress: 96,
+      status: "complicated",
+    },
+    {
+      firstName: "instrument",
+      lastName: "affair",
+      age: 15,
+      visits: 7,
+      progress: 41,
+      status: "relationship",
+    },
+    {
+      firstName: "border",
+      lastName: "comb",
+      age: 20,
+      visits: 5,
+      progress: 32,
+      status: "single",
+    },
+    {
+      firstName: "holiday",
+      lastName: "decision",
+      age: 3,
+      visits: 67,
+      progress: 42,
+      status: "complicated",
+    },
+  ];
+
   return (
     <Tabs className={styles["PanelTable"]}>
       <TabList className={styles["PanelTable-TabList"]}>
@@ -19,7 +97,7 @@ const PanelTable = () => {
       </TabList>
       <TabPanels className={styles["PanelTable-TabPanels"]}>
         <ActionFilters />
-        <Table />
+        <Table columns={columns} data={data} />
       </TabPanels>
     </Tabs>
   );
--- src/components/PanelTable/Table/Table.tsx
+++ src/components/PanelTable/Table/Table.tsx
@@ -1,25 +1,52 @@
+import { useTable, useBlockLayout } from "react-table";
+import { useSticky } from "react-table-sticky";
+
 import styles from "./Table.module.css";
 
-const Table = () => {
+const Table = ({ columns, data }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useBlockLayout,
+      useSticky
+    );
   return (
-    <div className={styles["Table"]}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div
+      {...getTableProps()}
+      className="table sticky"
+      style={{ maxWidth: 1000, height: 400 }}
+    >
+      <div className="header">
+        {headerGroups.map((headerGroup) => (
+          <div {...headerGroup.getHeaderGroupProps()} className="tr">
+            {headerGroup.headers.map((column) => (
+              <div {...column.getHeaderProps()} className="th">
+                {column.render("Header")}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
 
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div {...getTableBodyProps()} className="body">
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <div {...row.getRowProps()} className="tr">
+              {row.cells.map((cell) => {
+                return (
+                  <div {...cell.getCellProps()} className="td">
+                    {cell.render("Cell")}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
