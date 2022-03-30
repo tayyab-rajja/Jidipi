8536c297861ca0078bdf0ffc695109cbfe717466
tatianaKonovalova <tatiana.konovalova24@gmail.com>
2022-03-30 13:47:51 +0300

    created layout for share sidebar, logic for sending email and link copy

---
+++ src/components/ShareSidebar/ShareSidebar.tsx
@@ -0,0 +1,69 @@
+import { FC } from "react";
+import Link from 'next/link';
+
+import styles from './ShareSidebar.module.css';
+
+const ShareSidebar: FC = () => {
+
+    const copyLink = (e: any) => {
+        navigator.clipboard.writeText(e.target.textContent);
+    }
+
+    return (
+        <div className={styles["Sidebar"]}>
+            <div className={styles["Title"]}>share</div>
+            <ul className={styles["Links"]}>
+                <li className={styles["Links-Item"]}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]}>
+                        <Link href="#"><a>Facebook</a></Link>
+                    </span>
+                </li>
+                <li className={styles["Links-Item"]}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]}>
+                        <Link href="#"><a>Twitter</a></Link>
+                    </span>
+                </li>
+                <li className={styles["Links-Item"]}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]}>
+                        <Link href="#"><a>Instagram</a></Link>
+                    </span>
+                </li>
+                <li className={styles["Links-Item"]}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]}>
+                        <Link href="#"><a>Pinterest</a></Link>
+                    </span>
+                </li>
+                <li className={styles["Links-Item"]}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]}>
+                        <Link href="mailto:" passHref={true}><a>Email</a></Link>
+                    </span>
+                </li>
+                <li className={styles["Links-Item"]} onClick={copyLink}>
+                    <span className={styles["Links-Item_Icon"]}>
+                        {/* Icon to be added */}
+                    </span>
+                    <span className={styles["Links-Item_Text"]} >
+                        <Link href="#"><a>http://architecture.jidipi.com/j000012394/…</a></Link>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default ShareSidebar;
