d979cf8c833ae9f659d544f1998acaaebf240800
Danil <danil.zaychenko@headworks.com.ua>
2022-04-03 15:24:58 +0300

    dropdown added

--- src/components/PanelTable/ActionFilters/ActionFilters.tsx
+++ src/components/PanelTable/ActionFilters/ActionFilters.tsx
@@ -1,9 +1,66 @@
-import { Listbox, ListboxOption } from "@reach/listbox";
+import clsx from "clsx";
+import { MenuItem } from "@szhsin/react-menu";
+
+import { DropDown } from "../DropDownWithSearch/DropDown";
+
+import { categoriesSvg } from "constant/categoriesSvg";
 
 import styles from "./ActionFilters.module.css";
-import "@reach/listbox/styles.css";
-import { categoriesSvg } from "constant/categoriesSvg";
-import clsx from "clsx";
+
+const locationOptions = [
+  {
+    iconFlag: categoriesSvg["DATE"],
+    title: "TL",
+    text: "Text",
+  },
+  {
+    iconFlag: categoriesSvg["DATE"],
+    title: "AG",
+    text: "Lorem ipsum",
+  },
+  {
+    iconFlag: categoriesSvg["DATE"],
+    title: "MX",
+    text: "Country",
+  },
+];
+
+const actionOptions = [
+  {
+    icon: categoriesSvg["DATE"],
+    text: "Move",
+  },
+  {
+    icon: categoriesSvg["DATE"],
+    text: "Copy",
+  },
+  {
+    icon: categoriesSvg["DATE"],
+    text: "Delete",
+  },
+];
+
+const languageOptions = [
+  {
+    title: "TL",
+    text: "Text",
+  },
+  {
+    title: "AG",
+    text: "Lorem ipsum",
+  },
+  {
+    title: "MX",
+    text: "Country",
+  },
+];
+
+const allOptions = [
+  {
+    icon: categoriesSvg["DATE"],
+    text: "Move",
+  },
+];
 
 export const ActionFilters = () => {
   return (
@@ -17,9 +74,76 @@ export const ActionFilters = () => {
         >
           {/* TODO: set icon */}
           {categoriesSvg["DATE"]}
-          <Listbox arrow={<div className={styles["ActionFilters-Arrow"]} />}>
-            <ListboxOption value="action">Action</ListboxOption>
-          </Listbox>
+          <DropDown
+            className={styles["ActionFilters-Item_DropDown"]}
+            menuClassName={styles["ActionFilters-Item_Menu"]}
+            onChange={(value) => {}}
+            options={actionOptions}
+            renderOptions={({ icon, text }) => (
+              <MenuItem
+                value={text}
+                className={styles["ActionFilters-MenuItem"]}
+              >
+                <div>{icon}</div>
+                <div>{text}</div>
+              </MenuItem>
+            )}
+          />
+          <div className={styles["ActionFilters-Arrow"]} />
+        </div>
+
+        <div
+          className={clsx(
+            styles["ActionFilters-Item_Filter"],
+            styles["ActionFilters-Item"]
+          )}
+        >
+          {categoriesSvg["DATE"]}
+          <DropDown
+            className={styles["ActionFilters-Item_DropDown"]}
+            menuClassName={styles["ActionFilters-Item_Menu"]}
+            onChange={(value) => {}}
+            options={languageOptions}
+            renderOptions={({ title, text }) => (
+              <MenuItem
+                value={text}
+                className={styles["ActionFilters-MenuItem"]}
+              >
+                <div>{title}</div>
+                <div>{text}</div>
+              </MenuItem>
+            )}
+          />
+          <div className={styles["ActionFilters-Arrow"]} />
+        </div>
+
+        <div
+          className={clsx(
+            styles["ActionFilters-Item_Filter"],
+            styles["ActionFilters-Item"]
+          )}
+        >
+          {categoriesSvg["DATE"]}
+          <DropDown
+            className={styles["ActionFilters-Item_DropDown"]}
+            menuClassName={styles["ActionFilters-Item_MenuLocation"]}
+            onChange={(value) => {}}
+            options={locationOptions}
+            optionsPropsToFilter={["title", "text"]}
+            renderOptions={({ iconFlag, title, text }) => (
+              <MenuItem
+                value={text}
+                className={styles["ActionFilters-MenuItem"]}
+              >
+                <div>{categoriesSvg["DATE"]}</div>
+                <div>{iconFlag}</div>
+                <div>{title}</div>
+                <div>{text}</div>
+              </MenuItem>
+            )}
+            withSearch
+          />
+          <div className={styles["ActionFilters-Arrow"]} />
         </div>
 
         <div
@@ -30,41 +154,22 @@ export const ActionFilters = () => {
         >
           {/* TODO: set icon */}
           {categoriesSvg["DATE"]}
-          <Listbox arrow={<div className={styles["ActionFilters-Arrow"]} />}>
-            <ListboxOption value="value">
-              <span>11111111111111</span>
-            </ListboxOption>
-          </Listbox>
-        </div>
-
-        <div
-          className={clsx(
-            styles["ActionFilters-Item_Filter"],
-            styles["ActionFilters-Item"]
-          )}
-        >
-          {/* TODO: set icon */}
-          {categoriesSvg["DATE"]}
-          <Listbox arrow={<div className={styles["ActionFilters-Arrow"]} />}>
-            <ListboxOption value="value">
-              <span>11111111111111</span>
-            </ListboxOption>
-          </Listbox>
-        </div>
-
-        <div
-          className={clsx(
-            styles["ActionFilters-Item_Filter"],
-            styles["ActionFilters-Item"]
-          )}
-        >
-          {/* TODO: set icon */}
-          {categoriesSvg["DATE"]}
-          <Listbox arrow={<div className={styles["ActionFilters-Arrow"]} />}>
-            <ListboxOption value="value">
-              <span>11111111111111</span>
-            </ListboxOption>
-          </Listbox>
+          <DropDown
+            className={styles["ActionFilters-Item_DropDown"]}
+            menuClassName={styles["ActionFilters-Item_Menu"]}
+            onChange={(value) => {}}
+            options={allOptions}
+            renderOptions={({ icon, text }) => (
+              <MenuItem
+                value={text}
+                className={styles["ActionFilters-MenuItem"]}
+              >
+                <div>{icon}</div>
+                <div>{text}</div>
+              </MenuItem>
+            )}
+          />
+          <div className={styles["ActionFilters-Arrow"]} />
         </div>
       </div>
 
---
+++ src/components/PanelTable/DropDownWithSearch/DropDown.tsx
@@ -0,0 +1,77 @@
+import { FC, ReactElement, useEffect, useState } from "react";
+import { Menu, MenuItem, FocusableItem, MenuButton } from "@szhsin/react-menu";
+
+import "@szhsin/react-menu/dist/index.css";
+import styles from "./DropDown.module.css";
+import { categoriesSvg } from "constant/categoriesSvg";
+import clsx from "clsx";
+
+interface DropDownProps {
+  className?: string;
+  menuClassName?: string;
+  options: { [key: string]: any }[];
+  renderOptions: (agr: any) => ReactElement;
+  onChange: (value: any) => void;
+  withSearch?: boolean;
+  optionsPropsToFilter?: string[];
+}
+
+export const DropDown: FC<DropDownProps> = ({
+  menuClassName,
+  className,
+  options,
+  onChange,
+  renderOptions,
+  optionsPropsToFilter,
+  withSearch = false,
+}) => {
+  const [filter, setFilter] = useState("");
+  const [selectItem, setSelectItem] = useState("None");
+
+  return (
+    <Menu
+      menuClassName={clsx(styles["Menu"], menuClassName)}
+      menuButton={<MenuButton className={className}>{selectItem}</MenuButton>}
+      direction="bottom"
+      onItemClick={({ value }) => {
+        setSelectItem(value);
+        onChange(value);
+      }}
+      onMenuChange={(e) => {
+        e.open && setFilter("");
+      }}
+    >
+      {withSearch && (
+        <FocusableItem className={styles["Menu-Item"]}>
+          {({ ref }) => (
+            <div className={styles["Menu-InputContainer"]}>
+              <div className={styles["Menu-InputSearchIcon"]}>
+                {categoriesSvg["DATE"]}
+              </div>
+              <input
+                ref={ref}
+                className={styles["Menu-Input"]}
+                type="text"
+                placeholder="Type to filter"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value.trim())}
+              />
+            </div>
+          )}
+        </FocusableItem>
+      )}
+      {options
+        .filter((option) => {
+          if (!optionsPropsToFilter) {
+            return true;
+          }
+          return optionsPropsToFilter.some((name) => {
+            return option[name]
+              .toLocaleLowerCase()
+              .includes(filter.toLocaleLowerCase());
+          });
+        })
+        .map(renderOptions)}
+    </Menu>
+  );
+};
