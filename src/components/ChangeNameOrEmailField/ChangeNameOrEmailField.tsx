ad4c4e6b7aa08ccb6936f21b204fd61415d1029b
Alexey Shvets <alexey199822la@gmail.com>
2022-04-11 08:14:35 +0300

    creater put data functionality without hook

--- src/components/ChangeNameOrEmailField/ChangeNameOrEmailField.tsx
+++ src/components/ChangeNameOrEmailField/ChangeNameOrEmailField.tsx
@@ -1,12 +1,14 @@
 import {FC, useState} from 'react';
 
+import axios from 'axios'
+
 import ButtonUserData from 'src/components/ButtonUserData';
 import BarForInput from 'src/components/BarForInput';
 import InputUserData from 'src/components/InputUserData';
 import FormUserData from 'src/components/FormUserData';
 import NoValidationText from 'src/components/NoValidationText'
 
-import {usePutUserData} from 'src/api/usePutUserData'
+// import {usePutUserData} from 'src/api/usePutUserData'
 
 import styles from './ChangeNameOrEmailField.module.css'
 
@@ -43,15 +45,20 @@ const ChangeNameOrEmailField:FC = () => {
     }
   }
 
-  const putData = usePutUserData(null);
-
   const validateAndPostData = async () => {
     const {name, email} = inputsState
 
     if (name.value && /@/.test(email.value)) {
-      putData({
-        firstName: name.value,
-        email: email.value,
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Imdvb2dsZUVtYWlsIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJnb29nbGVJZCI6IjEwOTA0ODA4Njc5NTA3MzM2NjEzNCIsImZpcnN0TmFtZSI6IkRhbmlsIFpheWNoZW5rbyIsInVzZXJuYW1lIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJhdmF0YXIiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQVRYQUp5dDJvakRnS0lhUWFBc1AyckQzMF9BSDlfR1AzekRSaHdUcXhZNz1zOTYtYyIsImVtYWlsIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJpc1ZlcmlmaWVkIjp0cnVlLCJfaWQiOiI2MjRkYWFjNWNmOTE1MzAwMDk0OTg5MTYiLCJyb2xlcyI6W3siX2lkIjoiNjA3NzY3ZGRkM2U1ZWZmYzU1YjYxNTIyIiwidGl0bGUiOiJyZWFkZXIifV19LCJpYXQiOjE2NDkzMTc5NDgsImV4cCI6MTY1MTkwOTk0OH0.RHLV9Ovxawv7XRrgYSWOqoq-lR1SJRkZIGl60SjNnL0'
+
+      axios({
+        method: 'put',
+        url: `${process.env.NEXT_PUBLIC_API_URL}/user`,
+        headers: {Authorization: "Bearer " + token},
+        data: {
+          firstName: name.value,
+          email: email.value,
+        }
       })
       
       return;
