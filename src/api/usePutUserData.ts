ad4f6bf1f76d23dfe93d047ac13727e395affcfa
Alexey Shvets <alexey199822la@gmail.com>
2022-04-12 12:58:33 +0300

    can get user data by useSWR

--- src/api/usePutUserData.ts
+++ src/api/usePutUserData.ts
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { StringDecoder } from "string_decoder";
-// import { useAuth } from "src/providers/AuthProvider";
 import useSWR from "swr";
 
 interface Request {
@@ -11,21 +9,20 @@ interface Request {
   logoId?: string;
 }
 
-const url = `${process.env.NEXT_PUBLIC_API_URL}/user`
+const url = `${process.env.NEXT_PUBLIC_API_URL}/user/607767ddd3e5effc55b61522`
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Imdvb2dsZUVtYWlsIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJnb29nbGVJZCI6IjEwOTA0ODA4Njc5NTA3MzM2NjEzNCIsImZpcnN0TmFtZSI6IkRhbmlsIFpheWNoZW5rbyIsInVzZXJuYW1lIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJhdmF0YXIiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQVRYQUp5dDJvakRnS0lhUWFBc1AyckQzMF9BSDlfR1AzekRSaHdUcXhZNz1zOTYtYyIsImVtYWlsIjoiZGFuaWwuemF5Y2hlbmtvQGhlYWR3b3Jrcy5jb20udWEiLCJpc1ZlcmlmaWVkIjp0cnVlLCJfaWQiOiI2MjRkYWFjNWNmOTE1MzAwMDk0OTg5MTYiLCJyb2xlcyI6W3siX2lkIjoiNjA3NzY3ZGRkM2U1ZWZmYzU1YjYxNTIyIiwidGl0bGUiOiJyZWFkZXIifV19LCJpYXQiOjE2NDkzMTc5NDgsImV4cCI6MTY1MTkwOTk0OH0.RHLV9Ovxawv7XRrgYSWOqoq-lR1SJRkZIGl60SjNnL0'
 
-const fetcher = () => {
-  axios
+const fetcher = (url: string) => axios
     .get(url, { headers: { Authorization: "Bearer " + token } })
     .then((res) => res.data);
-}
+
 
 export const usePutUserData = () => {
   // const {
   //   session: { token },
   // } = useAuth();
 
-  const {data} = useSWR(null, fetcher);
+  const {data, error, isValidating} = useSWR(`${url}`, fetcher);
 
   const putData = (request: Request) => {
     axios({
@@ -35,5 +32,5 @@ export const usePutUserData = () => {
     });
   }
   
-  return {data, putData};
+  return {data, error, isValidating, putData};
 };
