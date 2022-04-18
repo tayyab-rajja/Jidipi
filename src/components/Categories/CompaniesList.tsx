c1650e4d63e399be96168339bb5d8a91feaf04d7
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2022-04-18 13:08:00 +0300

    created companies list

---
+++ src/components/Categories/CompaniesList.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { ICompany, ICompanyTab } from "types/categoryTypes";
+
+import styles from "./Categories.module.css";
+
+const List: FC = ({ children }) => {
+  return <ul className={styles["CategoriesList-List"]}>{children}</ul>;
+};
+
+const Item: FC<{
+  data: ICompany;
+}> = ({ data }) => {
+  const { query } = useRouter();
+
+  return (
+    <li>
+      <Link href={`/${query.folder}/categories/${data.partnerId}`}>
+        <a className={styles["CategoriesList-ItemLink"]}>
+          {data.title} <span>{data.count}</span>
+        </a>
+      </Link>
+    </li>
+  );
+};
+
+export const CompaniesList: FC<ICompanyTab> = ({
+  companies,
+  companiesCount,
+}) => {
+  //   const { query } = useRouter();
+
+  return (
+    <div className={styles.CategoriesList}>
+      <Link href={`#`}>
+        <a className={styles["CategoriesList-Header"]}>All {companiesCount}</a>
+      </Link>
+      <List>
+        {companies.map((company) => (
+          <Item key={company.partnerId} data={company} />
+        ))}
+      </List>
+    </div>
+  );
+};
