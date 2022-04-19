885c7377ac6523a0234ebb7ffef764ab4ff0f4eb
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2022-04-19 13:31:37 +0300

    added search for sidebbar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,41 @@
-import styles from "./Sidebar.module.css";
+import { useState } from "react";
+import qs from "qs";
+
+import { SearchInput } from "src/components/Input/SearchInput";
+import { useRouter } from "next/router";
 import Categories from "src/components/Categories";
+
+import { SearchInputValue } from "types/searcInputTypes";
+
+import styles from "./Sidebar.module.css";
 
 interface Props {}
 
 export const Sidebar = ({}: Props) => {
+  const { push, pathname, query } = useRouter();
+  const [search, setSearch] = useState<SearchInputValue>([]);
+
+  const handleSearch = (value: SearchInputValue) => {
+    setSearch(value);
+    let searchKey = value.map(({ value }) => value).join(" ");
+
+    push(
+      {
+        pathname,
+        query: { ...query, searchKey },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+
   return (
     <div className={styles["Sidebar"]}>
-      {/* <input
+      <SearchInput
         className={styles["Sidebar-SearchInput"]}
-        type="text"
-        // value={searchValue}
-        // onChange={handleSearch}
-        placeholder="Search"
-      /> */}
+        value={search}
+        onChange={handleSearch}
+      />
       <Categories />
     </div>
   );
