d37c331d298da8214dbcf5051afbda7ef3e2b41c
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2022-04-20 14:16:49 +0300

    added interface for request body

--- src/api/useLabels.ts
+++ src/api/useLabels.ts
@@ -1,36 +1,51 @@
-import axios from 'axios';
-import useSWR from 'swr';
+import axios from "axios";
+import useSWR from "swr";
 
-import { Label } from 'types/labelType';
+import { LabelBody } from "types/labelType";
 
 const fetcher = async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
-}
+  const response = await axios.get(url);
+  return response.data;
+};
 
 export const useLabels = () => {
+  const { data, isValidating, mutate } = useSWR(
+    [`${process.env.NEXT_PUBLIC_API_URL}/reader/labels`],
+    fetcher
+  );
 
-    const {data, isValidating, mutate} = useSWR([
-        `${process.env.NEXT_PUBLIC_API_URL}/reader/labels`], 
-        fetcher
+  const createLabel = async (body: LabelBody) => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/reader/labels`,
+      body
     );
+    const newLabel = response.data;
+    return mutate({ ...data, newLabel });
+  };
 
-    const createLabel = async (label: Label) => {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/reader/labels`, label)
-        const newLabel = response.data;
-        return mutate({...data, newLabel})
-    }
+  const updateLabel = async (body: LabelBody) => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/reader/labels`,
+      body
+    );
+    const updatedLabel = response.data;
+    return mutate({ ...data, updatedLabel });
+  };
 
-    const updateLabel = async (label: Label) => {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/reader/labels`, label);
-        const updatedLabel = response.data;
-        return mutate({...data, updatedLabel});
-    }
+  const deleteLabel = async (id: string) => {
+    await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/reader/label/${id}`);
+    return mutate({
+      labels: data.labels.filter(
+        (labelItem: LabelBody) => labelItem._id !== id
+      ),
+    });
+  };
 
-    const deleteLabel = async (id: string) => {
-        await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/reader/label/${id}`);
-        return mutate({labels: data.labels.filter((labelItem: Label) => labelItem._id !== id)});
-    }
-
-    return {labelsList: data?.labels, isValidating, createLabel, updateLabel, deleteLabel}
-}
+  return {
+    labelsList: data?.labels,
+    isValidating,
+    createLabel,
+    updateLabel,
+    deleteLabel,
+  };
+};
