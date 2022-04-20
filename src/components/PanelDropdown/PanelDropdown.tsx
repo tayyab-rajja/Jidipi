3263e8d5b5ca3309604884bcfa0cfe7a9be9179d
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2022-04-20 13:50:15 +0300

    added link to panel

--- src/components/PanelDropdown/PanelDropdown.tsx
+++ src/components/PanelDropdown/PanelDropdown.tsx
@@ -1,8 +1,9 @@
-import {FC} from 'react'
+import { FC } from "react";
 
-import clsx from 'clsx'
+import clsx from "clsx";
 
-import styles from './PanelDropdown.module.css'
+import styles from "./PanelDropdown.module.css";
+import Link from "next/link";
 
 interface Props {
   isOpen: boolean;
@@ -11,16 +12,33 @@ interface Props {
   logOut: () => void;
 }
 
-const PanelDropdown:FC<Props> = ({isOpen, setShowLoginBar, setShowSetting, logOut}) => {
-  return (isOpen
-    ? /* <div className={styles["Body-Wrapper"]} onClick={close}> */
-        <div className={clsx(styles["Container"], styles["Body-Container"])} onClick={() => setShowLoginBar(false)}>
-            <div className={styles["Container-List"]}>panel</div>
-            <div className={styles["Container-List"]} onClick={() => setShowSetting(true)}>setting</div>
-            <div className={styles["Container-List"]} onClick={logOut}>logout</div>
-        </div>
-      // </div>
-    : null)
-}
+const PanelDropdown: FC<Props> = ({
+  isOpen,
+  setShowLoginBar,
+  setShowSetting,
+  logOut,
+}) => {
+  return isOpen ? (
+    /* <div className={styles["Body-Wrapper"]} onClick={close}> */
+    <div
+      className={clsx(styles["Container"], styles["Body-Container"])}
+      onClick={() => setShowLoginBar(false)}
+    >
+      <Link href={"/panel"}>
+        <a className={styles["Container-List"]}>panel</a>
+      </Link>
+      <div
+        className={styles["Container-List"]}
+        onClick={() => setShowSetting(true)}
+      >
+        setting
+      </div>
+      <div className={styles["Container-List"]} onClick={logOut}>
+        logout
+      </div>
+    </div>
+  ) : // </div>
+  null;
+};
 
-export default PanelDropdown
+export default PanelDropdown;
