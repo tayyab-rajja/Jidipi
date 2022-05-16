2e16c04fd5631e149792917c1198e3f6907a3977
Tayyab Raja <tayyabraja316@gmail.com>
2022-05-16 18:35:04 +0500

    Conflicts Resolved

--- src/lib/common/api.tsx
+++ src/lib/common/api.tsx
@@ -1,80 +1,80 @@
-import {getJsonFromLocalStorage} from "./localStorage";
+import { getJsonFromLocalStorage } from "./localStorage";
 import useSWR from "swr";
 import axios from "axios";
-import {IFetchPostsResponse} from "../../api/fetchPosts";
-import {Cookies} from "react-cookie";
-import {GlobalUser} from "../../providers/UserProvider";
+import { IFetchPostsResponse } from "../../api/fetchPosts";
+import { Cookies } from "react-cookie";
+import { GlobalUser } from "../../providers/UserProvider";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? '';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
 const cookies = new Cookies();
-function generateHeader(){
-    const headers: any = {'Content-Type': 'application/json'}
-    if (GlobalUser.token) {
-        headers['Authorization'] = `Bearer ${GlobalUser.token}`
-    } else {
-        //TODO should use the middleware to check if the user is logged in
-        const token = cookies.get('token');
-        if (token) headers['Authorization'] = `Bearer ${token}`
-    }
-    return headers;
+function generateHeader() {
+  const headers: any = { "Content-Type": "application/json" };
+  if (GlobalUser.token) {
+    headers["Authorization"] = `Bearer ${GlobalUser.token}`;
+  } else {
+    //TODO should use the middleware to check if the user is logged in
+    const token = cookies.get("token");
+    if (token) headers["Authorization"] = `Bearer ${token}`;
+  }
+  return headers;
 }
 //
 // async function fetchAPI(query, {variables} = {}) {
 export async function POST(path: string, variables: any = {}): Promise<any> {
-    try {
-        const headers = generateHeader();
-        const res: Response = await fetch(API_URL + path, {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({...variables}),
-        })
+  try {
+    const headers = generateHeader();
+    const res: Response = await fetch(API_URL + path, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ ...variables }),
+    });
 
-        console.log(res);
-        if (!res.ok)      throw  new Error(res.statusText);
-        return await res.json();
-    } catch (e: any) {
-        throw  e;
-    }
+    console.log(res);
+    if (!res.ok) throw new Error(res.statusText);
+    return await res.json();
+  } catch (e: any) {
+    throw e;
+  }
 }
 export async function PUT(path: string, variables: any = {}): Promise<any> {
-    try {
-        const headers = generateHeader();
-        const res: Response = await fetch(API_URL + path, {
-            method: 'PUT',
-            headers,
-            body: JSON.stringify({...variables}),
-        })
-        if (!res.ok)      throw  new Error(res.statusText);
-        return await res.json();
-    } catch (e: any) {
-        throw  e;
-    }
+  try {
+    const headers = generateHeader();
+    const res: Response = await fetch(API_URL + path, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify({ ...variables }),
+    });
+    // if (!res.ok) throw new Error(res.statusText);
+    return await res.json();
+  } catch (e: any) {
+    throw e;
+  }
 }
 
 // export function* GET(path: string, variables: any = {}) {
 export async function GET(path: string): Promise<any> {
-    try {
-        const headers = generateHeader();
-        const res: Response = await fetch(API_URL + path, {
-            method: 'GET',
-            headers,
-        })
-        if (!res.ok)      throw  new Error(res.statusText);
-         return await res.json();
-    } catch (e: any) {
-        throw  e;
-    }
+  try {
+    const headers = generateHeader();
+    const res: Response = await fetch(API_URL + path, {
+      method: "GET",
+      headers,
+    });
+    // if (!res.ok)      throw  new Error(res.statusText);
+    return await res.json();
+  } catch (e: any) {
+    throw e;
+  }
 }
 export async function DELETE(path: string): Promise<any> {
-    try {
-        const headers = generateHeader();
-        const res: Response = await fetch(API_URL + path, {
-            method: 'DELETE',
-            headers,
-        })
-        if (!res.ok)      throw  new Error(res.statusText);
-         return await res.json();
-    } catch (e: any) {
-        throw  e;
-    }
+  try {
+    const headers = generateHeader();
+    const res: Response = await fetch(API_URL + path, {
+      method: "DELETE",
+      headers,
+    });
+    // if (!res.ok) throw new Error(res.statusText);
+    return await res.json();
+  } catch (e: any) {
+    throw e;
+  }
 }
--- src/pages/dashboard/post/list/[page].tsx
+++ src/pages/dashboard/post/list/[page].tsx
@@ -26,6 +26,8 @@ import {
 import { PageFolder } from "types/pageFolderType";
 import { FilterItem } from "constant/filters/interface";
 import { isJudge } from "src/lib/user/role";
+import { faThumbTack } from "@fortawesome/free-solid-svg-icons";
+import { animatedScrollTo } from "react-select/dist/declarations/src/utils";
 
 interface IProps {
     competitions: ICompetition[];
@@ -287,3 +289,169 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         props: props,
     };
 };
+
+// This is the done for the post in Post page I will remove that code and wil add in the post tab of cloud
+
+// import { GetServerSideProps, PreviewData } from "next";
+// import { GET } from "../../../../lib/common/api";
+// import useSWR from "swr";
+// import { useRouter } from "next/router";
+// import { FilterItem } from "../../../../lib/models/filter";
+// import Layout from "../../../../components/Layout";
+// import React, { useContext } from "react";
+// import SidebarDashboard from "../../../../components/Dashboard/Sidebar/SidebarDashboard";
+// import { UserContext } from "../../../../providers/UserProvider";
+// import { isJudge, isPartner } from "../../../../lib/user/role";
+// import Link from "next/link";
+// import { generateSidebarMenus } from "../../../../lib/common/menu";
+// import Navbar from "src/components/Navbar";
+// import SidebarDashboard2 from "src/components/Dashboard/Sidebar/SidebarDashboard2";
+// import styles from "src/components/Dashboard/Sidebar/SidebarDashboard.module.css";
+// import CloudContent from "src/components/Dashboard/CloudContent/CloudContent";
+// // import {generateSidebarMenus} from "../../../../lib/common/menu";
+// import { DashboardLayout } from "../../../../components/Dashboard/Layout/Layout";
+
+// export default function Posts(props: any) {
+//   const userContext: any = useContext(UserContext);
+//   const user = userContext.user;
+//   const router = useRouter();
+//   const { data, error } = useSWR(getKey(props), GET);
+//   if (error) return <div>error...</div>;
+//   if (!data) return <div>loading...</div>;
+//   console.log({ user, competitions: props.competitions });
+
+//   // MENU of the sidebar
+//   const menus = generateSidebarMenus({
+//     user,
+//     competitions: props.competitions,
+//   });
+
+//   //FILTER of the post list
+//   let filters: { [key: string]: FilterItem[] } = {};
+//   // PostStatus, MessageStatus, CoverStatus,
+//   // Filters for judge
+//   if (isJudge(user))
+//     filters = {
+//       award: [{ label: "abc", count: 1 }],
+//       score: [
+//         { label: "No Score", min: 0, max: 0 },
+//         { label: "Average Score 1-2", min: 1, max: 2 },
+//         { label: "Average Score 2-3", min: 2, max: 3 },
+//         { label: "Average Score 3-4", min: 3, max: 4 },
+//         { label: "Average Score 4-5", min: 4, max: 5 },
+//         { label: "Average Score 5-6", min: 5, max: 6 },
+//         { label: "Average Score 6-7", min: 6, max: 7 },
+//         { label: "Average Score 7-8", min: 7, max: 8 },
+//         { label: "Average Score 8-9", min: 8, max: 9 },
+//         { label: "Average Score 9-10", min: 9, max: 10 },
+//       ],
+//       comment: [],
+//     };
+//   //EOF FILTER of the post list
+//   const gotoPost = (id: string) => {
+//     router.push("/dashboard/post/" + id).then(() => {});
+//   };
+
+//   // return <Layout   sidebarComponent={
+//   //     <SidebarDashboard menus={menus}/>}>
+//   //     <div>
+//   //         <div>TOP header</div>
+//   //         <div>FILTERS here</div>
+//   //         <div>-----------------POST list</div>
+//   //         {data.posts && data.posts.map((post: any) => (
+//   //             <div key={post._id} className={styles.post}>
+//   //                 <Link href={'/dashboard/post/' + post._id}>
+//   //                     <div className={styles.post_title_text}>
+//   //                         <h2>{post.title}</h2>
+//   //                         <span>{post.publishedDate}</span>
+//   //                     </div>
+//   //                 </Link>
+//   //             </div>
+//   //         ))}
+//   //     </div>
+
+//   // </Layout>;
+
+//   return (
+//     <>
+//       <Navbar />
+//       <main>
+//         <section className={styles["main-section"]}>
+//           <div className={styles["main-section-inner"]}>
+//             <SidebarDashboard2 />
+//             <div className={styles["page-main-part"]}>
+//               <CloudContent />
+//             </div>
+//             <div>helk klrtrlt klertk</div>
+//           </div>
+//         </section>
+//       </main>
+//     </>
+//   );
+//   // return <DashboardLayout   sidebarComponent={
+//   //     <SidebarDashboard menus={menus}/>}>
+//   //     <div>
+//   //         <div>TOP header</div>
+//   //         <div>FILTERS here</div>
+//   //         <div>-----------------POST list</div>
+//   //         {data.posts && data.posts.map((post: any) => (
+//   //             <div key={post._id} className={styles.post}>
+//   //                 <Link href={'/dashboard/post/' + post._id}>
+//   //                     <div className={styles.post_title_text}>
+//   //                         <h2>{post.title}</h2>
+//   //                         <span>{post.publishedDate}</span>
+//   //                     </div>
+//   //                 </Link>
+//   //             </div>
+//   //         ))}
+//   //     </div>
+
+//   // </DashboardLayout>;
+// }
+
+// /**
+//  * Generate key for swr
+//  * @param props
+//  */
+// const getKey = (props: any) => {
+//   let query;
+//   if (props.query) {
+//     delete props.query.page;
+//     query = Object.keys(props.query)
+//       .map((key) => key + "=" + props.query[key])
+//       .join("&");
+//   }
+//   query = query ? "?" + query : "";
+//   return `/post/${props.currentPageFolder._id}/filterByPage${query}`;
+// };
+
+// /**
+//  * Get server side props
+//  * Load require data from API.
+//  * @param context
+//  */
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   //TODO check if user is logged in
+//   let props = {};
+//   try {
+//     // TODO combine the request into one call, or cache in redis...
+//     const pages = await GET("/pages");
+//     const pageFolders = pages && pages.pageFolders;
+//     const c = await GET("/competition");
+//     const currentPageFolder = pageFolders.find(
+//       (page: any) => context.params && page.subDomain === context.params.page
+//     );
+//     props = {
+//       competitions: c.competitions,
+//       pageFolders,
+//       currentPageFolder,
+//       query: context.query,
+//     };
+//   } catch (e) {}
+
+//   // Filter for staffs
+//   // editor,manager, image(CoverStatus)
+//   return {
+//     props: props,
+//   };
+// };
